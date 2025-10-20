6fe27bad4154e886186ec4bef3e25d208e70763a
Antti Tikka <antti.tikka@hotmail.com>
2025-10-20 22:45:11 +0300

    Remove unnecessary eslint ignore

--- patientor/backend/src/services/patientService.ts
+++ patientor/backend/src/services/patientService.ts
@@ -19,7 +19,6 @@ const getNonSensitiveEntires = (): NonSensitivePatient[] => {
 };
 
 const addPatient = (patient: NewPatient): Patient => {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
   const id: string = uuid();
   const newPatient = {
     id,
